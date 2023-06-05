d7c50cc5941538ef5deb57ae3dc3b8552a95f58f
Abbey Nelson <62493355+abbeynels@users.noreply.github.com>
2023-06-05 14:45:18 -0600

    Update auth-example.sql

--- examples/auth-example.sql
+++ examples/auth-example.sql
@@ -16,10 +16,11 @@ GO
 ALTER DATABASE [AuthDB] SET TRUSTWORTHY ON
 GO
 
+-- Only create url if you are not using the default value (https://us-street.api.smartystreets.com/street-address)
 CREATE TABLE Auth (
 	auth_id nvarchar(max),
 	auth_token nvarchar(max),
-  -- url nvarchar(max)
+  	-- url nvarchar(max)
 	)
 
 INSERT INTO Auth VALUES('auth_id', 'auth_token' /*, ''*/))
