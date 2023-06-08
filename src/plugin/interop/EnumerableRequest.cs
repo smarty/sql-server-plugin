bd479f338c22898adcb7526e13be5a5188343f3b
Abbey Nelson <62493355+abbeynels@users.noreply.github.com>
2023-06-08 10:16:21 -0600

    Organized code

---
+++ src/plugin/interop/EnumerableRequest.cs
@@ -0,0 +1,39 @@
+﻿namespace SmartySqlServerPlugin.Interop
+{
+    using System;
+    using System.Collections;
+    using SmartyStreets;
+
+    public abstract class EnumerableRequest<TLookup, TCandidate> : IEnumerable
+        where TCandidate : class, new()
+    {
+        private readonly IClient<TLookup> _client;
+        protected readonly TLookup Lookup;
+
+        protected EnumerableRequest(IClient<TLookup> client, TLookup lookup)
+        {
+            _client = client;
+            Lookup = lookup;
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            try
+            {
+                _client.Send(Lookup);
+                return Response();
+            }
+            catch (Exception ex)
+            {
+                var status = ex.ParseStatus();
+                if (status == 0)
+                    throw;
+
+                return Response(status);
+            }
+        }
+
+        protected abstract EnumerableResponse<TCandidate> Response();
+        protected abstract EnumerableResponse<TCandidate> Response(int status);
+    }
+}
---
+++ src/plugin/interop/USStreetResponse.cs
@@ -0,0 +1,16 @@
+﻿namespace SmartySqlServerPlugin.Interop
+{
+    using System.Collections.Generic;
+    using SmartyStreets.USStreetApi;
+
+    public class USStreetResponse : EnumerableResponse<Candidate>
+    {
+        public USStreetResponse(IList<Candidate> candidates) : base(candidates)
+        {
+        }
+
+        public USStreetResponse(int status) : base(status)
+        {
+        }
+    }
+}
